c611f1c02390bee388106409670a82b857e8b689
Aiden068 <aiden.biggs@my.jcu.edu.au>
2025-10-11 15:32:04 +1000

    Print a grid of numbers with a selected amount of rows

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,13 @@
 import random
-random_numbers = 6
+number_of_columns = 6
 i = 0
 quick_picks = int(input("How many quick picks? "))
-while i < random_numbers:
-    i += 1
-    print(random.randint(1, 45), "")
+for i in range(quick_picks):
+    numbers = []
+    while len(numbers) < number_of_columns:
+        random_number = random.randint(1, 45)
+
+    # list1 = list1.append(random.randint(1, 45))
+        if random_number not in numbers:
+            numbers.append(random_number)
+    print(" ".join(str(f"{number:3}") for number in numbers))
