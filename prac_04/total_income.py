25bfa394bdbb085b6760e7510db296a3541613f3
Aiden068 <aiden.biggs@my.jcu.edu.au>
2025-10-11 15:36:19 +1000

    Get a report on income over a given number of months

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -11,19 +11,14 @@ def print_report(amount_of_months, incomes):
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
-
-
-
 def main():
-    """Display income report for incomes over a given number of amount_of_months."""
+    """Display income report for incomes over a given number of months."""
     incomes = []
-    amount_of_months = int(input("How many amount_of_months? "))
+    amount_of_months = int(input("How many months? "))
 
     for month in range(1, amount_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
-
     print_report(amount_of_months, incomes)
-
 
 main()
