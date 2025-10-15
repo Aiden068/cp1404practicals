fa31ebfc83c56c8d075d7f44cbdaf2e44d8eb26b
Aiden068 <aiden.biggs@my.jcu.edu.au>
2025-10-15 13:04:24 +1000

    Completed code

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,6 +1,10 @@
-
+word_occurrence = {}
 user_text = input("Enter a sentence: ")
 words = user_text.split()
-print(words)
+
 for word in words:
-    print(len(words[word]))
+    frequency = word_occurrence.get(word, 0)
+    word_occurrence[word] = frequency + 1
+
+for word in sorted(word_occurrence):
+    print(f"{word:{max(len(word) for word in words)}} : {word_occurrence[word]}")
